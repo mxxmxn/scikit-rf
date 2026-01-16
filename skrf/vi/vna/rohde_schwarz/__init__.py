3928ba6b1651751f759f2b8e423db31342298ba3
Maximilian Martin <maximilian_martin@outlook.de>
2026-01-16 09:12:43 +0100

    Fix tests for Rohde & Schwarz VNAs

--- skrf/vi/vna/rohde_schwarz/__init__.py
+++ skrf/vi/vna/rohde_schwarz/__init__.py
@@ -5,5 +5,7 @@ Rohde & Schwarz VNAs (:mod:`skrf.vi.vna.rohde_schwarz`)
 =======================================================
 
 """
+
+from .rs_vna import AveragingMode, SweepMode, SweepType
 from .zna import ZNA
 from .zva import ZVA
